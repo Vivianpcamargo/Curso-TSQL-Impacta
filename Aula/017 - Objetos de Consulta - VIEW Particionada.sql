6a989075716ece1f89950e4265a42487acd1afdc
Vivianpcamargo <vivianpaulacamargo@gmail.com>
2022-05-30 17:38:43 -0300

    update: Aula 17

---
+++ Aula/017 - Objetos de Consulta - VIEW Particionada.sql
@@ -0,0 +1,20 @@
+USE SisDep;
+-- CHECK OPTION (Restrição de Entrada de Dados)
+GO
+CREATE VIEW vw_NovaLocalidadeSP
+AS
+	SELECT idLocalidade,Uf,Cidade FROM Localidade
+	WHERE Uf = 'SP'
+WITH CHECK OPTION;
+GO 
+-- Testando
+-- Vai dar erro
+INSERT INTO vw_NovaLocalidadeSP
+(idLocalidade,Uf,Cidade)
+VALUES
+(12,'RJ','Parati');
+--Vai Funcionar
+INSERT INTO vw_NovaLocalidadeSP
+(idLocalidade,Uf,Cidade)
+VALUES
+(12,'SP','Ribeirão Preto');
